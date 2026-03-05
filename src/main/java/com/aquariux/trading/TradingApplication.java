b6b233167827585313ab9250abd3ef1ba04967f7
thomas.ldt <thomas.tld@byteuptech.com>
2026-03-05 13:20:31 +0700

    Enable Swagger

--- src/main/java/com/aquariux/trading/TradingApplication.java
+++ src/main/java/com/aquariux/trading/TradingApplication.java
@@ -1,9 +1,20 @@
 package com.aquariux.trading;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Crypto Trading API",
+				version = "1.0",
+				description = "Crypto trading system with price aggregation, trading, wallet and transaction APIs"
+		)
+)
 public class TradingApplication {
 
 	public static void main(String[] args) {
